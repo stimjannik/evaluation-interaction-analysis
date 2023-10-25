e68b608a6a884b4a860d1cdca24ed7be70e1654a
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-10-25 18:12:27 +0200

    Update run scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
 #! /bin/bash
-config_dir='config_minimal'
-
-java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/prepare.properties 
-java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/interactions_01.properties 
-java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/interactions_02.properties
-#java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/interactions_03.properties
-java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/interactions_04.properties
-java -jar build/libs/evaluation-interaction-analysis-0.1.0-SNAPSHOT-all.jar --config ${config_dir}/general.properties,${config_dir}/run.properties 
+./config_default/run.sh
